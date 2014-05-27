c65d58085ba14953fe89796f771f730e7d7d3862
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-27 23:31:49 +0100

    Can load user's files

--- lib/report.rb
+++ lib/report.rb
@@ -1,7 +1,14 @@
 require 'json'
+require 'rest_client'
 
 class Report
   def json(response)
     JSON.parse(response)
+  end
+
+  def list_files(username, password, apikey)
+    response =  RestClient.post 'https://my.workshare.com/api/open-v1.0/user_sessions.json', "user_session[email]" => username, "user_session[password]" => password, "device[app_uid]" => apikey
+    resource = RestClient::Resource.new("https://my.workshare.com/api/open-v1.0/files.json", :cookies => response.cookies)
+    resource.get(:accept => 'application/json')
   end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -3,6 +3,15 @@ require "report"
 
 describe Report do
   let(:report) {Report.new}
+
+  it "can load a user's files" do
+    username = ENV['TEST_USERNAME']
+    password = ENV['TEST_PASSWORD']
+    apikey = ENV['TEST_APIKEY']
+    response = report.list_files(username, password, apikey)
+    expect(report.json(response)).to have_key('files')
+  end
+
   it "can parse the response" do
     response = File.read("spec/sample_response.json")
     expect(report.json(response)).to have_key('files')
