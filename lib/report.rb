abe2b2970618da7e16f27d8447aaba7aa1229679
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-28 23:03:30 +0100

    Added dotenv if rack environment is development

--- lib/report.rb
+++ lib/report.rb
@@ -3,13 +3,18 @@ require 'rest_client'
 require_relative "user_file"
 require 'active_support/inflector'
 
+if ENV['RACK_ENV'] == 'development'
+  require 'dotenv'
+  Dotenv.load
+end
+
 class Report
   def json(response)
     JSON.parse(response)
   end
 
   def list_files(username, password)
-    response =  RestClient.post 'https://my.workshare.com/api/open-v1.0/user_sessions.json', "user_session[email]" => username, "user_session[password]" => password, "device[app_uid]" => ENV['WORKSHARE_API_KEY']
+    response = RestClient.post 'https://my.workshare.com/api/open-v1.0/user_sessions.json', "user_session[email]" => username, "user_session[password]" => password, "device[app_uid]" => ENV['WORKSHARE_API_KEY']
     resource = RestClient::Resource.new("https://my.workshare.com/api/open-v1.0/files.json", :cookies => response.cookies)
     resource.get(:accept => 'application/json')
   end
