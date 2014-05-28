bdf93f74ff7501e9aee58fafde8ea81b889f13bb
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-28 02:06:01 +0100

    Refactored

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -3,22 +3,21 @@ require "report"
 
 describe Report do
   let(:report) {Report.new}
+  let(:response){ File.read("spec/sample_response.json") }
 
   it "can load a user's files" do
     username = ENV['TEST_USERNAME']
     password = ENV['TEST_PASSWORD']
     apikey = ENV['TEST_APIKEY']
-    response = report.list_files(username, password, apikey)
-    expect(report.json(response)).to have_key('files')
+    response2 = report.list_files(username, password, apikey)
+    expect(report.json(response2)).to have_key('files')
   end
 
   it "can parse the response" do
-    response = File.read("spec/sample_response.json")
     expect(report.json(response)).to have_key('files')
   end
 
   it "can extract files from the parsed json" do
-    response = File.read("spec/sample_response.json")
     parsed = report.json(response)
     files = report.extract_files(parsed)
     expect(files.count).to eq 7
