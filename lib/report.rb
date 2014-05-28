07d998ed134b4d6e059649e68c198c0ed17a03cf
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-28 02:40:28 +0100

    Report can extract categories with their particular data

--- lib/report.rb
+++ lib/report.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'rest_client'
+require "user_file"
 
 class Report
   def json(response)
@@ -14,5 +15,18 @@ class Report
 
   def extract_files(parsed)
     parsed["files"].map{|file| UserFile.new(file["extension"], file["size"])}
+  end
+
+  def extract_categories(files)
+    categories = Hash.new
+    files.each do |file|
+      if categories[file.category]
+        categories[file.category][:files_count] += 1
+        categories[file.category][:total_weight] += file.weight
+      else
+        categories[file.category] = {files_count: 1, total_weight: file.weight}
+      end
+    end
+    categories
   end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -4,6 +4,8 @@ require "report"
 describe Report do
   let(:report) {Report.new}
   let(:response){ File.read("spec/sample_response.json") }
+  let(:parsed) { report.json(response) }
+  let(:files) { report.extract_files(parsed) }
 
   it "can load a user's files" do
     username = ENV['TEST_USERNAME']
@@ -18,10 +20,13 @@ describe Report do
   end
 
   it "can extract files from the parsed json" do
-    parsed = report.json(response)
-    files = report.extract_files(parsed)
     expect(files.count).to eq 7
     expect(files[0].category).to eq "Videos"
     expect(files[0].weight).to eq 14336.00
+  end
+
+  it "can return a hash of hashes with details about each category" do
+    result = {files_count: 2, total_weight: 43008.00}
+    expect(report.extract_categories(files)["Videos"]).to eq result
   end
 end
