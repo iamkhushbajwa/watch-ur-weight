8edec5be2131230c1599a09d52b80be32c3d7317
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-28 20:27:00 +0100

    Adds report to session variable

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require "sinatra"
 require_relative "lib/report"
+require 'dotenv'
+Dotenv.load
 
 enable :sessions
 set :session_secret, 'sharethatwork'
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  report = Report.new
+  response = report.list_files(params[:username], params[:password], ENV['WORKSHARE_API_KEY'])
+  parsed = report.json(response)
+  files = report.extract_files(parsed)
+  categories = report.extract_categories(files)
+  displacement = report.displacement(categories, files)
+  total_weight = report.total_weight(categories)
+  session[:report] = report.result(categories, displacement, total_weight)
+  redirect '/'
 end
